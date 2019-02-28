8e804184e05bb9f22752807fac23e518ee3ed9e3
NDOLIC <tblaguese1@gmail.com>
2019-02-28 09:17:49 +0200

    adding upvote

--- app/main/views.py
+++ app/main/views.py
@@ -1,79 +1,5 @@
 
-# from flask import render_template,redirect,url_for,abort,request
-# from . import main
-# from flask_login import login_required,current_user
-# from ..models import User,Comment,Pitch
-# from .forms import UpdateProfile,CommentForm,PitchForm
-# from .. import db,photos
 
-
-
-# @main.route('/')
-# def index():
-
-#     '''
-#     View root page function that returns the index page and its data
-#     '''
-
-#     message = 'Hello World'
-#     return render_template('index.html',message = message)
-
-
-# @main.route('/movie/<int:id>')
-# def movie(id):
-
-#     '''
-#     View movie page function that returns the movie details page and its data
-#     '''
-#     movie = get_movie(id)
-#     title = f'{movie.title}'
-#     reviews = Review.get_reviews(movie.id)
-
-#     return render_template('movie.html',title = title,movie = movie,reviews = reviews)
-
-
-
-# @main.route('/user/<uname>')
-# def profile(uname):
-#     user = User.query.filter_by(username = uname).first()
-
-#     if user is None:
-#         abort(404)
-
-#     return render_template("profile/profile.html", user = user)
-
-
-
-# @main.route('/user/<uname>/update',methods = ['GET','POST'])
-# @login_required
-# def update_profile(uname):
-#     user = User.query.filter_by(username = uname).first()
-#     if user is None:
-#         abort(404)
-
-#     form = UpdateProfile()
-
-#     if form.validate_on_submit():
-#         user.bio = form.bio.data
-
-#         db.session.add(user)
-#         db.session.commit()
-
-#         return redirect(url_for('.profile',uname=user.username))
-
-#     return render_template('profile/update.html',form =form)
-
-
-# @main.route('/user/<uname>/update/pic',methods= ['POST'])
-# @login_required
-# def update_pic(uname):
-#     user = User.query.filter_by(username = uname).first()
-#     if 'photo' in request.files:
-#         filename = photos.save(request.files['photo'])
-#         path = f'photos/{filename}'
-#         user.profile_pic_path = path
-#         db.session.commit()
-#     return redirect(url_for('main.profile',uname=uname))
 from flask import render_template, request, redirect, url_for, abort
 from . import main
 from .forms import PitchForm, CommentForm, Vote  
@@ -175,3 +101,15 @@ def update_pic(uname):
         user.profile_pic_path = path
         db.session.commit()
     return redirect(url_for('main.profile', uname=uname))
+
+
+def single_pitch(id):
+    pitch=Pitch.query.filter_by(id=id).first()
+    comments=Comment.get_comments(id=id)
+    return render_template('pitch.html',pitch=pitch,comments=comments)
+
+@main.route('/downvotes/<int:id>')
+def upvoting(id):
+    pitch1=Pitch.query.filter_by(id=id).first()
+    pitch1.upvotes=Pitch.upvote(id)
+    return redirect(url_for('main.single_pitch',pitch=pitch1.upvotes))
