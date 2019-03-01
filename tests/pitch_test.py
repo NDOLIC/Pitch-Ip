0e4717f6793682ef23e2cd3ed6c8e499ef4747bf
NDOLIC <tblaguese1@gmail.com>
2019-03-01 12:36:53 +0200

    changing css

--- tests/pitch_test.py
+++ tests/pitch_test.py
@@ -1,56 +1,36 @@
 import unittest
-from app.models import Movie
-
-# class MovieTest(unittest.TestCase):
-#     '''
-#     Test Class to test the behaviour of the Movie class
-#     '''
-
-#     def setUp(self):
-#         '''
-#         Set up method that will run before every Test
-#         '''
-#         self.new_movie = Movie(1234,'Python Must Be Crazy','A thrilling new Python Series','/khsjha27hbs',8.5,129993)
-
-#     def test_instance(self):
-#         self.assertTrue(isinstance(self.new_movie,Movie))
-
-# if __name__ == '__main__':
-#     unittest.main()
-import unittest
-from app.models import Pitch, User
-# from flask_login import current_user
-# from app import db
+from app.models import Pitch,User
+from app import db
 
 
-class TestPitch(unittest.TestCase):
 
-    def setUp(self):
-        self.user = User(
-            username='', password='', email='')
-        self.new_pitch = Pitch(title='',body="",author='',category='',upvotes=,downvotes=,user=self.user)
+class PitchModelTest(unittest.TestCase):
+   def setUp(self):
+        self.user_James = User(username = 'claire',password = 'food', email = 'tblaguese1@gmail.com', bio='Good',profile_pic_path='https://sss.com')
+        self.new_pitch = Pitch(id=1234,content='Pitch ',category="hhh",upvotes=12,downvotes=34,user = self.user_James )
 
-    def tearDown(self):
-        Pitch.query.delete()
+   def tearDown(self):
         User.query.delete()
+        Pitch.query.delete()
 
-    def test_instance(self):
-        self.assertTrue(isinstance(self.new_pitch, Pitch))
 
-    def test_check_instance_variables(self):
-        self.assertEquals(
-            self.new_pitch.title, 'Elevator Pitch Example for an Professional Accountant')
-        self.assertEquals(self.new_pitch.body, "Test Pitch")
-        self.assertEquals(self.new_pitch.author,
-                          'Improv Andy')
-        self.assertEquals(self.new_pitch.category,
-                          'business')
-        self.assertEquals(self.new_pitch.upvotes,
-                          1)
-        self.assertEquals(self.new_pitch.downvotes,
-                          0)
-        self.assertEquals(self.new_pitch.user, self.user_James)
+   def test_check_instance_variables(self):
+        self.assertEquals(self.new_pitch.id,1234)
+        self.assertEquals(self.new_pitch.content,'Pitch ')
+        self.assertEquals(self.new_pitch.category,"hhh")
+        self.assertEquals(self.new_pitch.upvotes,2)
+        self.assertEquals(self.new_pitch.downvotes,4)
+        self.assertEquals(self.new_pitch.user,self.user_James)
 
-    def test_save_pitch(self):
-        self.new_pitch.save_pitches()
-        self.assertTrue(len(Pitch.query.all()) > 0)
+   def test_save_pitch(self):
+        self.new_pitch.save_pitch()
+        self.assertTrue(len(Pitch.query.all())>0)  
+
+   def test_get_pitch_by_id(self):
+
+        self.new_pitch.save_pitch()
+        got_pitchs = Pitch.get_pitche(1234)
+        self.assertTrue(len(got_pitchs) == 1)    
+
+if __name__ == '__main__':
+    unittest.main()
